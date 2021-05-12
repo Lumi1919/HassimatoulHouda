0e1a473fac0580f3ffaa5d4ff31121dd012c2d65
Lumi1919 <82605764+Lumi1919@users.noreply.github.com>
2021-05-12 16:53:55 +0200

    Update urls.py

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -8,4 +10,6 @@ urlpatterns = [
     path('membres/', include('membres.urls')),
     path('event/', include('event.urls')),
     path('admin/', admin.site.urls)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL)
