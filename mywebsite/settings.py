5fa5eb63791b4d02b176a2fe5199810b343e534a
Lumi1919 <82605764+Lumi1919@users.noreply.github.com>
2021-05-12 17:14:39 +0200

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['os.path.join(BASE_DIR)],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
