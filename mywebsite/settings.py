b93b71b00d9c32cedb57ce8bf6a77ade72f51211
Lumi1919 <82605764+Lumi1919@users.noreply.github.com>
2021-05-14 18:15:23 +0200

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-elvj6nw1rdiv!fa09k#&6l5e_pocmf0ytve*c0^t8%b*0r%x^8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['assimatoul-houda.herokuapp.com']
 
 
 # Application definition
